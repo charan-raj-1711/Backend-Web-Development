7360d471275c3566df3b9e1324c035333d502915
Your Name <charan.a1711@gmail.com>
2026-09-25 14:02:05 +0530

    Created postRepository, refactored postService

---
+++ Module 4/Repository Pattern/src/repositories/postRepository.js
@@ -0,0 +1,59 @@
+const postStore = require('../data/postStore');
+
+function findAll() {
+    return [...postStore.posts];
+}
+
+function findById(id) {
+    return postStore.posts.find(
+        (post) => post.id === Number(id) 
+    ) || null;
+}
+
+function create(fields) {
+    const post = {
+        id: postStore.nextId(),
+        title: fields.title,
+        body: fields.body || '',
+        authorId: fields.authorId,
+    };
+
+    postStore.posts.push(post);
+
+    return post;
+}
+
+function update(id, patch){
+    const post =  findById(id);
+
+    if(!post) return null;
+
+    if(patch.title !== undefined){
+        post.title = patch.title;
+    }
+    if(patch.body !== undefined){
+        post.body = patch.body;
+    }
+
+    return post;
+}
+
+function remove(id){
+    const index = postStore.findByIndex(
+        (post) => post.id === Number(id)
+    );
+
+    if(index === -1) return false;
+
+    postStore.posts.splice(index,1);
+
+    return true;
+}
+
+module.exports ={
+    findAll,
+    findById,
+    create,
+    update,
+    remove,
+};
--- Module 4/Repository Pattern/src/services/postService.js
+++ Module 4/Repository Pattern/src/services/postService.js
@@ -1,43 +1,54 @@
-const postStore = require('../data/postStore');
+// const postStore = require('../data/postStore');
+const postRepository = require('../repositories/postRepository');
 
 function listPosts() {
-  return postStore.posts;
+  // return postStore.posts;
+  return postRepository.findAll();
 }
 
 function getPost(id) {
-  return postStore.posts.find((post) => post.id === Number(id)) || null;
+  // return postStore.posts.find((post) => post.id === Number(id)) || null;
+  return postRepository.findById(id);
 }
 
 function createPost(fields) {
-  if (!fields || !fields.title) {
+  // if (!fields || !fields.title) {
+  //   const error = new Error('Title is required');
+  //   error.statusCode = 422;
+  //   throw error;
+  // }
+
+  // const post = {
+  //   id: postStore.nextId(),
+  //   title: fields.title,
+  //   body: fields.body || '',
+  //   authorId: fields.authorId,
+  // };
+  // postStore.posts.push(post);
+  // return post;
+  if(!fields || !fields.title) {
     const error = new Error('Title is required');
     error.statusCode = 422;
     throw error;
   }
-
-  const post = {
-    id: postStore.nextId(),
-    title: fields.title,
-    body: fields.body || '',
-    authorId: fields.authorId,
-  };
-  postStore.posts.push(post);
-  return post;
+  return postRepository.create(fields);
 }
 
 function updatePost(id, patch) {
-  const post = getPost(id);
-  if (!post) return null;
-  if (patch.title !== undefined) post.title = patch.title;
-  if (patch.body !== undefined) post.body = patch.body;
-  return post;
+  // const post = getPost(id);
+  // if (!post) return null;
+  // if (patch.title !== undefined) post.title = patch.title;
+  // if (patch.body !== undefined) post.body = patch.body;
+  // return post;
+  return postRepository.update(id, patch);
 }
 
 function removePost(id) {
-  const index = postStore.posts.findIndex((post) => post.id === Number(id));
-  if (index === -1) return false;
-  postStore.posts.splice(index, 1);
-  return true;
+  // const index = postStore.posts.findIndex((post) => post.id === Number(id));
+  // if (index === -1) return false;
+  // postStore.posts.splice(index, 1);
+  // return true;
+  return postRepository.remove(id);
 }
 
 module.exports = { listPosts, getPost, createPost, updatePost, removePost };
